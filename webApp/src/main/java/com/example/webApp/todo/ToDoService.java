3d6ecec89ad0090128116fe4057ac609196284ca
Randimal441 <randimalchamika@gmail.com>
2025-06-26 22:35:37 +0530

    update todo service

--- webApp/src/main/java/com/example/webApp/todo/ToDoService.java
+++ webApp/src/main/java/com/example/webApp/todo/ToDoService.java
@@ -12,14 +12,20 @@ import com.example.webApp.controller.newLoginController;
 public class ToDoService {
 
 	private static List<ToDo> todos = new ArrayList<>();
+	
+	private static int todosCount = 0;
 	static {
-		todos.add(new ToDo(1, "in28min", "Learn Was", LocalDate.now().plusYears(1), false));
+		todos.add(new ToDo(, "in28min", "Learn Was", LocalDate.now().plusYears(1), false));
 		todos.add(new ToDo(1, "in28min", "Learn ABC", LocalDate.now().plusYears(2), false));
 		todos.add(new ToDo(1, "in28min", "Learn DEF", LocalDate.now().plusYears(3), false));
 	}
 	
 	public List<ToDo> findByUsername(String username){
 		return todos;
+	}
+	public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
+		
+		
 	}
 	
 }
