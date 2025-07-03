6349546f73fb05bcade82e63071f78152cbd1530
Randimal441 <randimalchamika@gmail.com>
2025-07-03 23:16:37 +0530

    Update SpringSecurityConfiguration.java

--- webApp/src/main/java/com/example/webApp/security/SpringSecurityConfiguration.java
+++ webApp/src/main/java/com/example/webApp/security/SpringSecurityConfiguration.java
@@ -1,5 +1,26 @@
 package com.example.webApp.security;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
 public class SpringSecurityConfiguration {
+	//InMemoryUserDetailsManager
+	//inMemoryUserDetailsManager(UserDetails... users)
+	
+	@Bean
+	public InMemoryUserDetailsManager createUserDetailsManager() {
+		UserDetails userDetails = User.withDefaultPasswordEncoder()
+		.username("in28min")
+		.password("dummy")
+		.roles("USER","ADMIN")
+		.build();
+		
+		return new InMemoryUserDetailsManager(userDetails);
+	}
 
 }
