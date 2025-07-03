ae06cb242f50337d04d91b04ed9bbe5f28890392
Randimal441 <randimalchamika@gmail.com>
2025-07-03 23:44:50 +0530

    Update SpringSecurityConfiguration.java

--- webApp/src/main/java/com/example/webApp/security/SpringSecurityConfiguration.java
+++ webApp/src/main/java/com/example/webApp/security/SpringSecurityConfiguration.java
@@ -1,10 +1,14 @@
 package com.example.webApp.security;
 
 
+import java.util.function.Function;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -14,13 +18,21 @@ public class SpringSecurityConfiguration {
 	
 	@Bean
 	public InMemoryUserDetailsManager createUserDetailsManager() {
-		UserDetails userDetails = User.withDefaultPasswordEncoder()
-		.username("in28min")
-		.password("dummy")
-		.roles("USER","ADMIN")
-		.build();
+		
+		Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
+		
+		UserDetails userDetails = User.builder()
+				.passwordEncoder(passwordEncoder)
+				.username("in28min")
+				.password("dummy")
+				.roles("USER","ADMIN")
+				.build();
 		
 		return new InMemoryUserDetailsManager(userDetails);
 	}
-
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
